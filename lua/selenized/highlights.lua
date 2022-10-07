63d4896d74be9aecd61df55a7c03bb45ad351a73
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2022-10-07 16:22:43 -0400

    Add missing diff highlight groups

--- lua/selenized/highlights.lua
+++ lua/selenized/highlights.lua
@@ -17,66 +17,6 @@ local theme = lush(function()
     Bold           { gui = "bold" },
     Italic         { gui = "italic" },
 
-    Error          { fg = palette.br_red }, -- (preferred) any erroneous construct
-
-    Normal         { bg = palette.bg_0, fg = palette.fg_0 },
-    Comment        { fg = palette.dim_0 },
-    NormalFloat    { bg = palette.bg_1, fg = Normal.fg }, -- Normal text in floating windows.
-    FloatBorder       { bg = palette.bg_1, fg = palette.dim_0 }, -- Border of floating windows
-    -- Conceal        { }, -- placeholder characters substituted for concealed text (see 'conceallevel')
-    Cursor         { Reverse }, -- character under the cursor
-    -- lCursor        { }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
-    -- CursorIM       { }, -- like Cursor, but used when in IME mode |CursorIM|
-    Directory      { fg = palette.cyan }, -- directory names (and other special names in listings)
-    DiffAdd        { bg = palette.bg_1, fg = palette.green}, -- diff mode: Added line |diff.txt|
-    DiffChange     { bg = palette.bg_1, fg = palette.yellow }, -- diff mode: Changed line |diff.txt|
-    DiffDelete     { bg = palette.bg_1, fg = palette.red}, -- diff mode: Deleted line |diff.txt|
-    DiffText       { fg = palette.red, Reverse }, -- diff mode: Changed text within a changed line |diff.txt|
-    -- TermCursor     { }, -- cursor in a focused terminal
-    -- TermCursorNC   { }, -- cursor in an unfocused terminal
-    ErrorMsg       { Error }, -- error messages on the command line
-    -- VertSplit      { }, -- the column separating vertically split windows
-    Folded         { bg = palette.bg_1, fg = palette.dim_0 }, -- line used for closed folds
-    ColorColumn    { bg = palette.bg_1 }, -- used for the columns set with 'colorcolumn'
-    FoldColumn     { bg = palette.bg_1, fg = palette.fg_0 }, -- 'foldcolumn'
-    SignColumn     { bg = palette.bg_0 }, -- column where |signs| are displayed
-    CursorColumn   { bg = palette.bg_1 }, -- used for the columns set with 'cursorcolumn'
-    CursorLine     { bg = palette.bg_1 }, -- used for the row set with 'cursorline'
-    CursorLineNr   { fg = palette.br_violet, bg = palette.bg_1 }, -- used for the row set with 'cursorline'
-    LineNr         { Comment },
-    -- Substitute     { }, -- |:substitute| replacement text highlighting
-    MatchParen     { bg = palette.bg_2 }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
-    -- ModeMsg        { }, -- 'showmode' message (e.g., "-- INSERT -- ")
-    -- MsgArea        { }, -- Area for messages and cmdline
-    -- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
-    -- MoreMsg        { }, -- |more-prompt|
-    NonText        { Comment }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-    -- EndOfBuffer    { }, -- filler lines (~) after the end of the buffer.  By default, this is highlighted like |hl-NonText|.
-    -- NormalNC       { }, -- normal text in non-current windows
-    Pmenu          { bg = palette.bg_1, Normal }, -- Popup menu: normal item.
-    PmenuSel       { bg = palette.bg_2 }, -- Popup menu: selected item.
-    PmenuSbar      { bg = palette.bg_2 }, -- Popup menu: scrollbar.
-    PmenuThumb     { bg = palette.fg_0 }, -- Popup menu: Thumb of the scrollbar.
-    Question       { Normal }, -- |hit-enter| prompt and yes/no questions
-    -- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-    SpecialKey     { fg = palette.br_cyan, gui = Italic.gui }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
-    -- SpellBad       { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
-    -- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
-    -- SpellLocal     { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
-    -- SpellRare      { }, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
-    -- StatusLine     { }, -- status line of current window
-    -- StatusLineNC   { }, -- status lines of not-current windows Note: if this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
-    -- TabLine        { }, -- tab pages line, not active tab page label
-    -- TabLineFill    { }, -- tab pages line, where there are no labels
-    -- TabLineSel     { }, -- tab pages line, active tab page label
-    Title          { fg = palette.br_violet }, -- titles for output from ":set all", ":autocmd" etc.
-    Visual         { bg = palette.bg_2 }, -- Visual mode selection
-    -- VisualNOS      { }, -- Visual mode selection when vim is "Not Owning the Selection".
-    WarningMsg     { fg = palette.br_red }, -- warning messages
-    Whitespace     { Comment }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
-    WildMenu       { Visual }, -- current match in 'wildmenu' completion
-    Search         { fg = palette.bg_0, bg = palette.br_yellow },
-
     -- These groups are not listed as default vim groups,
     -- but they are defacto standard group names for syntax highlighting.
     -- commented out groups should chain up to their "preferred" group by
@@ -123,6 +63,79 @@ local theme = lush(function()
     -- Ignore         { }, -- (preferred) left blank, hidden  |hl-Ignore|
 
     Todo           { fg = palette.br_red, Standout }, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+    Error          { fg = palette.br_red }, -- (preferred) any erroneous construct
+
+    Normal         { bg = palette.bg_0, fg = palette.fg_0 },
+    Comment        { fg = palette.dim_0 },
+    NormalFloat    { bg = palette.bg_2, fg = Normal.fg }, -- Normal text in floating windows.
+    FloatBorder       { bg = palette.bg_2, fg = palette.dim_0 }, -- Border of floating windows
+    -- Conceal        { }, -- placeholder characters substituted for concealed text (see 'conceallevel')
+    Cursor         { Reverse }, -- character under the cursor
+    -- lCursor        { }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
+    -- CursorIM       { }, -- like Cursor, but used when in IME mode |CursorIM|
+    Directory      { fg = palette.cyan }, -- directory names (and other special names in listings)
+
+    -- Git
+    diffFile       { fg = palette.fg_1, Bold },
+    diffIndexLine  { Identifier, gui = Italic.gui },
+    diffAdded      { fg = palette.green}, -- diff mode: Added line |diff.txt|
+    DiffAdd        { diffAdded },
+    diffChanged    { fg = palette.yellow }, -- diff mode: Changed line |diff.txt|
+    DiffChange     { diffChanged },
+    diffRemoved    { fg = palette.red}, -- diff mode: Deleted line |diff.txt|
+    DiffDelete     { diffRemoved },
+    DiffText       { fg = palette.red, Reverse }, -- diff mode: Changed text within a changed line |diff.txt|
+    diffLine       { Constant },
+    diffSubname    { Normal },
+    diffOldFile    { bg = palette.bg_1, fg = DiffDelete.fg },
+    diffNewFile    { bg = palette.bg_1, fg = DiffAdd.fg },
+
+    -- TermCursor     { }, -- cursor in a focused terminal
+    -- TermCursorNC   { }, -- cursor in an unfocused terminal
+    ErrorMsg       { Error }, -- error messages on the command line
+    -- VertSplit      { }, -- the column separating vertically split windows
+
+    Folded         { bg = palette.bg_1, fg = palette.dim_0 }, -- line used for closed folds
+    ColorColumn    { bg = palette.bg_1 }, -- used for the columns set with 'colorcolumn'
+    FoldColumn     { bg = palette.bg_1, fg = palette.fg_0 }, -- 'foldcolumn'
+    SignColumn     { bg = palette.bg_0 }, -- column where |signs| are displayed
+    CursorColumn   { bg = palette.bg_1 }, -- used for the columns set with 'cursorcolumn'
+    CursorLine     { bg = palette.bg_1 }, -- used for the row set with 'cursorline'
+    CursorLineNr   { fg = palette.br_violet, bg = palette.bg_1 }, -- used for the row set with 'cursorline'
+    LineNr         { Comment },
+
+    -- Substitute     { }, -- |:substitute| replacement text highlighting
+    MatchParen     { bg = palette.bg_2 }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+    -- ModeMsg        { }, -- 'showmode' message (e.g., "-- INSERT -- ")
+    -- MsgArea        { }, -- Area for messages and cmdline
+    -- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
+    -- MoreMsg        { }, -- |more-prompt|
+    NonText        { Comment }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+    -- EndOfBuffer    { }, -- filler lines (~) after the end of the buffer.  By default, this is highlighted like |hl-NonText|.
+    -- NormalNC       { }, -- normal text in non-current windows
+    Pmenu          { bg = palette.bg_1, Normal }, -- Popup menu: normal item.
+    PmenuSel       { bg = palette.bg_2 }, -- Popup menu: selected item.
+    PmenuSbar      { bg = palette.bg_2 }, -- Popup menu: scrollbar.
+    PmenuThumb     { bg = palette.fg_0 }, -- Popup menu: Thumb of the scrollbar.
+    Question       { Normal }, -- |hit-enter| prompt and yes/no questions
+    -- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
+    SpecialKey     { fg = palette.br_cyan, gui = Italic.gui }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
+    -- SpellBad       { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
+    -- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
+    -- SpellLocal     { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
+    -- SpellRare      { }, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
+    -- StatusLine     { }, -- status line of current window
+    -- StatusLineNC   { }, -- status lines of not-current windows Note: if this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+    -- TabLine        { }, -- tab pages line, not active tab page label
+    -- TabLineFill    { }, -- tab pages line, where there are no labels
+    -- TabLineSel     { }, -- tab pages line, active tab page label
+    Title          { fg = palette.br_violet }, -- titles for output from ":set all", ":autocmd" etc.
+    Visual         { bg = palette.bg_2 }, -- Visual mode selection
+    -- VisualNOS      { }, -- Visual mode selection when vim is "Not Owning the Selection".
+    WarningMsg     { fg = palette.br_red }, -- warning messages
+    Whitespace     { Comment }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
+    WildMenu       { Visual }, -- current match in 'wildmenu' completion
+    Search         { fg = palette.bg_0, bg = palette.br_yellow },
 
     -- These groups are for the native LSP client. Some other LSP clients may use
     -- these groups, or use their own. Consult your LSP client's documentation.
@@ -153,7 +166,7 @@ local theme = lush(function()
     DiagnosticUnderlineError             { DiagnosticError, gui = Underlined.gui },
     DiagnosticUnderlineWarn              { DiagnosticWarn, gui = Underlined.gui },
     DiagnosticUnderlineInfo              { DiagnosticInfo, gui = Underlined.gui },
-    DiagnosticUnderlineHint              { DiagnosticHint, bg = palette.bg_1 },
+    DiagnosticUnderlineHint              { DiagnosticHint, gui = Underlined.gui },
     LspDiagnosticsVirtualTextError       { LspDiagnosticsError },
     LspDiagnosticsVirtualTextWarning     { LspDiagnosticsWarning },
     LspDiagnosticsVirtualTextInformation { LspDiagnosticsInformation },
@@ -189,8 +202,8 @@ local theme = lush(function()
     -- TSMethod             { }, -- For method calls and definitions.
     -- TSField              { }, -- For fields.
     -- TSProperty           { }, -- Same as `TSField`.
-    TSConstructor        { fg = palette.violet }, -- For constructor calls and definitions: `{ }` in Lua, and Java constructors.
-    TSConditional        { fg = palette.magenta }, -- For keywords related to conditionals.
+    TSConstructor        { Type }, -- For constructor calls and definitions: `{ }` in Lua, and Java constructors.
+    -- TSConditional        { }, -- For keywords related to conditionals.
     -- TSRepeat             { }, -- For keywords related to loops.
     -- TSLabel              { }, -- For labels: `label:` in C and `:label:` in Lua.
     -- TSOperator           { }, -- For any operator: `+`, but also `->` and `*` in C.
@@ -224,7 +237,7 @@ local theme = lush(function()
     CmpItemKindText           { fg = Normal.fg },
     CmpItemKindMethod         { Special },
     CmpItemKindFunction       { CmpItemKindMethod },
-    CmpItemKindConstructor    { TSConstructor },
+    CmpItemKindConstructor    { Type },
     -- CmpItemKindField          { },
     CmpItemKindVariable       { Identifier },
     CmpItemKindClass          { Type },
